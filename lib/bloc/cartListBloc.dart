4a98b7d2ab4e760c207f70c093b0515f46267b3c
Ghezae Goitom <ggappsww@gmail.com>
2020-09-02 23:57:14 +0300

    first version of functional app

---
+++ lib/bloc/cartListBloc.dart
@@ -0,0 +1,36 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:rxdart/rxdart.dart';
+import '../model/foodItem.dart';
+import 'provider.dart';
+
+//brodcast 11.37
+
+class CartListBloc extends BlocBase {
+  CartListBloc();
+
+//Stream that receives a number and changes the count;
+  var _listController = BehaviorSubject<List<FoodItem>>.seeded([]);
+//output
+  Stream<List<FoodItem>> get listStream => _listController.stream;
+//input
+  Sink<List<FoodItem>> get listSink => _listController.sink;
+
+//Bussiness Logic
+
+  CartProvider provider = CartProvider();
+
+//for adding food to the list
+  void addToList(FoodItem foodItem) =>
+      _listController.add(provider.addToList(foodItem));
+
+//for removing food from the list
+  void removeFromList(FoodItem foodItem) =>
+      _listController.add(provider.removeFoodItem(foodItem));
+
+  @override
+  void dispose() {
+    // will be called automatically
+    _listController.close();
+    super.dispose();
+  }
+}
